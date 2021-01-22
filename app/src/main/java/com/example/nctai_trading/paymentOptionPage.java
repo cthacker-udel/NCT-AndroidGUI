4422010d1bbdd6467016fbd9ed027d99288348dc
cthacker-udel <cthacker@udel.edu>
2021-01-22 14:58:33 -0500

    Added square functionality, updated jarRepositories and also updated app minimum sdk to 21, and also updated payment option page pay by card feature to initiate square functionality.

--- app/src/main/java/com/example/nctai_trading/paymentOptionPage.java
+++ app/src/main/java/com/example/nctai_trading/paymentOptionPage.java
@@ -6,11 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import sqip.Card;
+import sqip.CardDetails;
+import sqip.CardEntry;
 
 public class paymentOptionPage extends AppCompatActivity {
 
     Button termsOfServiceBtn;
     Button backToHomePageBtn;
+    Button payByCardBtn;
 
     // TODO: [PAYMENT OPTION PAGE] Implement check discount code button feature
     // TODO: [PAYMENT OPTION PAGE] Implement pay by crypto button feature
@@ -23,12 +29,19 @@ public class paymentOptionPage extends AppCompatActivity {
         setContentView(R.layout.activity_payment_option_page);
         termsOfServiceBtn = findViewById(R.id.paymentOptionTermsOfServiceBtn);
         backToHomePageBtn = findViewById(R.id.backToHomePageButton);
+        payByCardBtn = findViewById(R.id.payByCardBtn);
         termsOfServiceBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent toTermsOfService = new Intent(getApplicationContext(),termsOfServicePage.class);
                 startActivity(toTermsOfService);
             }
+        });
+
+        payByCardBtn.setOnClickListener((view) ->
+        {
+            CardEntry.startCardEntryActivity(paymentOptionPage.this,true,
+            CardEntry.DEFAULT_CARD_ENTRY_REQUEST_CODE);
         });
 
         backToHomePageBtn.setOnClickListener(new View.OnClickListener() {
@@ -38,5 +51,21 @@ public class paymentOptionPage extends AppCompatActivity {
                 startActivity(toHomePage);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        CardEntry.handleActivityResult(data,result -> {
+            if(result.isSuccess()){
+                CardDetails cardResult = result.getSuccessValue();
+                Card card = cardResult.getCard();
+                String nonce = cardResult.getNonce();
+            }
+            else if(result.isCanceled()){
+                Toast.makeText(paymentOptionPage.this,"Canceled",Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
