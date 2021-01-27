ae3c08873d3d4af7a5a1dc00d13b0ab23861a0ab
cthacker-udel <cthacker@udel.edu>
2021-01-26 21:23:14 -0500

    upadted ar page

--- app/src/main/java/com/example/nctai_trading/ARPage.java
+++ app/src/main/java/com/example/nctai_trading/ARPage.java
@@ -2,13 +2,27 @@ package com.example.nctai_trading;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ARPage extends AppCompatActivity {
+
+    Button ARButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_a_r_page);
+        ARButton = findViewById(R.id.ARPageButton);
+
+        ARButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent backToMainPage = new Intent(getApplicationContext(),mainPage.class);
+                startActivity(backToMainPage);
+            }
+        });
     }
 }
