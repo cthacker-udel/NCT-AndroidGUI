94fb56e93e23feda393af594bf3479f410b39e5c
cthacker-udel <cthacker@udel.edu>
2021-02-05 00:39:02 -0500

    update

--- app/src/main/java/com/example/nctai_trading/paymentOptionPage.java
+++ app/src/main/java/com/example/nctai_trading/paymentOptionPage.java
@@ -4,8 +4,6 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
-import android.content.ActivityNotFoundException;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -16,7 +14,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.example.nctai_trading.squareSDK.cardEntryBackgroundHandler;
 import com.mongodb.BasicDBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
@@ -64,7 +61,6 @@ public class paymentOptionPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_option_page);
 
-        com.example.nctai_trading.squareSDK.cardEntryBackgroundHandler cardEntryBackgroundHandler;
         //CardEntry.setCardNonceBackgroundHandler(cardEntryBackgroundHandler);
 
         termsOfServiceBtn = findViewById(R.id.paymentOptionTermsOfServiceBtn);
@@ -151,19 +147,5 @@ public class paymentOptionPage extends AppCompatActivity {
         });
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        CardEntry.handleActivityResult(data,result -> {
-            if(result.isSuccess()){
-                CardDetails cardResult = result.getSuccessValue();
-                Card card = cardResult.getCard();
-                String nonce = cardResult.getNonce();
-            }
-            else if(result.isCanceled()){
-                Toast.makeText(paymentOptionPage.this,"Canceled",Toast.LENGTH_SHORT).show();
-                return;
-            }
-        });
-    }
+
 }
