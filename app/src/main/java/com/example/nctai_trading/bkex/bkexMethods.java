ecb325da657925c29a2e0f8c8b7c61cbdfac03fd
cthacker-udel <cthacker@udel.edu>
2021-03-13 10:11:29 -0500

    added bkex Order implementation

--- app/src/main/java/com/example/nctai_trading/bkex/bkexMethods.java
+++ app/src/main/java/com/example/nctai_trading/bkex/bkexMethods.java
@@ -5,20 +5,25 @@ import android.os.Build;
 import androidx.annotation.RequiresApi;
 
 import com.example.nctai_trading.HMAC256;
+import com.example.nctai_trading.bkex.order.placeOrderResponse;
 
+import java.io.IOException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.TreeMap;
 
+import retrofit2.Call;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class bkexMethods {
 
-    String apikey = "";
-    String secretKey = "";
-    String baseUrl = "https://api.bkex.vip";
+    String apikey = "7deb2a0c2e0801c4e3a24ba7fb2a425a01552c3d120c98b5ce054ba87b53d237";
+    String secretKey = "f575bf4218d62f0f328cf61a5a514bbc4f7fca095999133954675d014a2390ac";
+    String baseUrl = "https://api.bkex.com";
 
     public String getSignature(String params) throws InvalidKeyException, NoSuchAlgorithmException {
         HMAC256 hmac256 = new HMAC256();
@@ -26,7 +31,7 @@ public class bkexMethods {
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
-    public String generateQueryString(Map<String,String> params){
+    public String generateQueryString(Map<String,Object> params){
 
         ArrayList<String> paramList = new ArrayList<>();
         for(String eachKey: params.keySet()){
@@ -53,6 +58,42 @@ public class bkexMethods {
 
         }
 
+
+
+    }
+
+    public class orderRequests{
+
+        @RequiresApi(api = Build.VERSION_CODES.O)
+        public placeOrderResponse createOrder(String pair, Double price, Double amount) throws NoSuchAlgorithmException, InvalidKeyException, IOException {
+
+            String url = baseUrl + "/v1/u/trade/order/create/";
+
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(url)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+
+            bkexOrderInterface bkexOrderInterface = retrofit.create(com.example.nctai_trading.bkex.bkexOrderInterface.class);
+
+            Map<String,Object> params = new TreeMap<>();
+
+            params.put("pair",pair);
+            params.put("price",price);
+            params.put("amount",amount);
+
+            String signature = getSignature(generateQueryString(params));
+
+            Call<placeOrderResponse> call = bkexOrderInterface.placeOrder(params,apikey,signature);
+
+            Response<placeOrderResponse> response = call.execute();
+
+            return response.body();
+
+
+
+
+        }
 
 
     }
