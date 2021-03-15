a663f82e280eb81a2f03747ed75341011522e56b
cthacker-udel <cthacker@udel.edu>
2021-03-15 10:52:35 -0400

    added add keys page

---
+++ app/src/main/java/com/example/nctai_trading/adminMethods/adminMethods.java
@@ -0,0 +1,14 @@
+package com.example.nctai_trading.adminMethods;
+
+import java.util.ArrayList;
+
+public class adminMethods {
+
+    ArrayList<String> listOfAllExchanges = new ArrayList<>();
+
+    public static void addUserExchanges(){
+
+    }
+
+
+}
