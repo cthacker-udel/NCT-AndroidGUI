f2f9221484d15d153397f183bc5641f272c359f1
cthacker-udel <cthacker@udel.edu>
2021-03-20 06:42:41 -0400

    added buy currency page currency and exchange selector, and the sell currency page exchange and currency selector.

--- app/src/main/java/com/example/nctai_trading/buyCurrencyPage.java
+++ app/src/main/java/com/example/nctai_trading/buyCurrencyPage.java
@@ -1,33 +1,47 @@
 package com.example.nctai_trading;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 import com.example.nctai_trading.R;
 
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
 public class buyCurrencyPage extends AppCompatActivity {
 
     Spinner buyCurrencySpinner;
+    Spinner buyCurrencyExchangeSpinner;
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buy_currency_page);
         buyCurrencySpinner = findViewById(R.id.buyCurrencySpinner);
 
-
+        buyCurrencyExchangeSpinner = findViewById(R.id.buyCurrencyExchangeSpinner);
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item);
         adapter.add("---- select currency ----");
 
-        exchangesList exchangeListMethods = new exchangesList();
+        currencyInfo currencyInfo = new currencyInfo();
 
-        adapter.addAll(exchangeListMethods.getExchangesList());
+        adapter.addAll(com.example.nctai_trading.currencyInfo.currencyList().keySet().toArray(new String[0]));
 
         buyCurrencySpinner.setAdapter(adapter);
+
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item);
+
+        adapter2.add("---- select exchange ----");
+        adapter2.addAll(new exchangesList().getExchangesList());
+
+        buyCurrencyExchangeSpinner.setAdapter(adapter2);
 
     }
 }
--- app/src/main/java/com/example/nctai_trading/sellPage.java
+++ app/src/main/java/com/example/nctai_trading/sellPage.java
@@ -1,30 +1,45 @@
 package com.example.nctai_trading;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 public class sellPage extends AppCompatActivity {
 
-    Spinner sellCurrencyPage;
-    Spinner buyCurrencyExchanges;
+    Spinner sellCurrencyCurrencySpinner;
+    Spinner sellCurrencyPageExchanges;
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sell_page);
 
-        sellCurrencyPage = findViewById(R.id.sellCurrencySpinner);
+        sellCurrencyCurrencySpinner = findViewById(R.id.sellCurrencySpinner);
 
-        buyCurrencyExchanges = findViewById(R.id.sellPageExchangeSpinner);
+        sellCurrencyPageExchanges = findViewById(R.id.sellPageExchangeSpinner);
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item);
 
         adapter.add("---- select currency ----");
         exchangesList exchangesList = new exchangesList();
-        adapter.addAll(exchangesList.getExchangesList());
+        currencyInfo currencyInfo = new currencyInfo();
+        adapter.addAll(currencyInfo.currencyList().keySet().stream().toArray(String[]::new));
+
+        sellCurrencyCurrencySpinner.setAdapter(adapter);
+
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item);
+
+        adapter2.add("---- select exchange ----");
+        adapter2.addAll(exchangesList.getExchangesList());
+
+        sellCurrencyPageExchanges.setAdapter(adapter2);
+
+
 
     }
 }
