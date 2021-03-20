949e75b4ce3e018b03635fd8624e19e27e354da2
cthacker-udel <cthacker@udel.edu>
2021-03-20 07:06:06 -0400

    added sell currency sell amount adapter

--- app/src/main/java/com/example/nctai_trading/sellPage.java
+++ app/src/main/java/com/example/nctai_trading/sellPage.java
@@ -8,10 +8,13 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import java.util.stream.IntStream;
+
 public class sellPage extends AppCompatActivity {
 
     Spinner sellCurrencyCurrencySpinner;
     Spinner sellCurrencyPageExchanges;
+    Spinner sellCurrencyAmount;
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
@@ -19,9 +22,11 @@ public class sellPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sell_page);
 
-        sellCurrencyCurrencySpinner = findViewById(R.id.sellCurrencySpinner);
+        sellCurrencyCurrencySpinner = findViewById(R.id.sellCurrencySpinner2);
 
         sellCurrencyPageExchanges = findViewById(R.id.sellPageExchangeSpinner);
+
+        sellCurrencyAmount = findViewById(R.id.sellCurrencySpinner2);
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item);
 
@@ -38,6 +43,12 @@ public class sellPage extends AppCompatActivity {
         adapter2.addAll(exchangesList.getExchangesList());
 
         sellCurrencyPageExchanges.setAdapter(adapter2);
+
+        ArrayAdapter<String> adapter3 = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item);
+
+        adapter3.addAll(IntStream.range(1,999).mapToObj(String::valueOf).toArray(String[]::new));
+
+        sellCurrencyAmount.setAdapter(adapter3);
 
 
 
