59d80b9fe0187e49b520a8ea3fca5381d5c1c468
cthacker-udel <cthacker@udel.edu>
2021-04-13 06:09:53 -0400

    Updated reboot handler that handles if the phone is powered off, and powered back on, the notification service is started

--- app/src/main/java/com/example/nctai_trading/particleRebootHandler.java
+++ app/src/main/java/com/example/nctai_trading/particleRebootHandler.java
@@ -3,12 +3,17 @@ package com.example.nctai_trading;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
 
 public class particleRebootHandler extends BroadcastReceiver {
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onReceive(Context context, Intent intent) {
         if(intent.getAction().equals("android.intent.action.BOOT_COMPLETED")){
-            // set the alarm here
+            mainPage mainPage = new mainPage();
+            mainPage.startService();
         }
     }
 }
